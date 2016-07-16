474f3214fa4c1f8258a6c89bff63ed1e41f63590
SplittyDev <splittydev@gmail.com>
2016-07-16 22:18:41 +0200

    Add a basic GDT setup

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -4,6 +4,20 @@ section .text
 bits 32
 start:
   call setup_paging
+  lgdt [gdt64.pointer]
+  mov word [0xb8000], 0x0248 ; H
+  mov word [0xb8002], 0x0265 ; e
+  mov word [0xb8004], 0x026c ; l
+  mov word [0xb8006], 0x026c ; l
+  mov word [0xb8008], 0x026f ; o
+  mov word [0xb800a], 0x022c ; ,
+  mov word [0xb800c], 0x0220 ;
+  mov word [0xb800e], 0x0277 ; w
+  mov word [0xb8010], 0x026f ; o
+  mov word [0xb8012], 0x0272 ; r
+  mov word [0xb8014], 0x026c ; l
+  mov word [0xb8016], 0x0264 ; d
+  mov word [0xb8018], 0x0221 ; !
   hlt
 
 setup_paging:
@@ -55,3 +69,34 @@ page_directory_pointer_table:
   resb 4096
 page_directory_table:
   resb 4096
+
+section .rodata
+; Defines
+GDT_BIT_READWRITE   equ 41
+GDT_BIT_EXECUTABLE  equ 43
+GDT_BIT_TYPE        equ 44
+GDT_BIT_PRESENT     equ 47
+GDT_BIT_LONG        equ 53
+; Global Descriptor Table
+gdt64:
+  ; Zero segment
+  .zero equ $ - gdt64:
+    dq 0
+  ; Code segment
+  .code: equ $ - gdt64
+    dq \
+      (1 << GDT_BIT_TYPE)       |\
+      (1 << GDT_BIT_PRESENT)    |\
+      (1 << GDT_BIT_READWRITE)  |\
+      (1 << GDT_BIT_EXECUTABLE) |\
+      (1 << GDT_BIT_LONG)
+  ; Data segment
+  .data: equ $ - gdt64
+    dq \
+      (1 << GDT_BIT_TYPE)     |\
+      (1 << GDT_BIT_PRESENT)  |\
+      (1 << GDT_BIT_READWRITE)
+  ; GDT pointer
+  .pointer:
+    dw .pointer - gdt64 - 1
+    dq gdt64
